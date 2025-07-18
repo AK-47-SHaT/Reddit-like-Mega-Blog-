c2437c1f0675c78daf44d6f097c96f64ff8ff3ad
Akshat Chauhan <akshataaryan511@gmail.com>
2025-07-18 22:57:22 +0530

    setting up redux toolkit and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
+import React , {useState,useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+
 
 function App() {
   
- console.log(import.meta.env.VITE_APPWRITE_URL);
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
+
+ useEffect(() => {
+   authService.getCurrentUser()
+   .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+   })
+   .finally(()=>setLoading(false))
  
+   
+ }, [])
+ 
+
   return (
     <>
+    <div className='bg-amber-300'>HOI</div>
      A blog with appwrite
     </>
   )
