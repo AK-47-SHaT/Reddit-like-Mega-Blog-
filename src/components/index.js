d253e63abfa52bf666ca87c95185fb4c3547c0e6
Akshat Chauhan <akshataaryan511@gmail.com>
2025-07-20 18:54:03 +0530

    added components (Input , PostCard, Login, signup)

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,17 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import Logo from "./Logo";
+import Container from "./Container/Container";
+import Button from "./Button";
+import Input from "./Input";
 
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    Container,
+    Button,
+    Input,
+
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import React, { StrictMode } from 'react';
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { BrowserRouter } from 'react-router-dom';
 
 
 import './index.css'
@@ -9,8 +10,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
     <App />
     </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
