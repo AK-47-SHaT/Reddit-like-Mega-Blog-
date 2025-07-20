b09ab9a74227372cef24a68120daa999250fc098
Akshat Chauhan <akshataaryan511@gmail.com>
2025-07-21 00:35:30 +0530

    added pages and components

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,11 @@ import Logo from "./Logo";
 import Container from "./Container/Container";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
+import Login from "./Login";
+import Signup from "./Signup";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/Postform";
 
 export {
     Header,
@@ -12,6 +17,14 @@ export {
     Container,
     Button,
     Input,
+    RTE,
+    Login,
+    Signup,
+    PostCard,
+    PostForm,
+    
+
+
 
 
 }
